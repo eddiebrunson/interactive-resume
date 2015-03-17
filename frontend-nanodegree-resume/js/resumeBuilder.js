b011456abce6d0057822a7aec2d9eca1d11d3818
Edward "Eddie" Brunson <eddiebrunson@gmail.com>
2015-03-16 23:39:02 -0400

    Committed by Udacity

---
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -0,0 +1,441 @@
+var bio = {
+	"name": "Edward Brunson",
+	"role": "Front-End Web Developer",
+	"contacts": {
+		"email": "webdev.eddie@gmail.com",
+		"github": "edwardbrunson",
+		"twitter": "@webdev_eddie",
+		"location": "Ashburn, Virginia, United States"
+	},
+	"welcomeMessage": "I'm currently studying Web Development and teaching myself to code!",
+	"skills": ["Python", "JavaScript", "HTML", "CSS", "jQuery", "Responsive Design", "Motivated Learner", "Leadership"],
+	"bioPic": "images/FullSizeRender.jpg"
+};
+
+var education = {
+	"schools": [
+	  { "name": "Virginia Commonwealth University",
+	  	"location": "Richmond, Virginia, United States",
+	  	"degree": ["B.S. Real Estate and Urban Land Development, B.S. Computer Science, B.S. Poltical Science"],
+	  	"majors": ["Real Estate, Computer Science, Political Science"],
+	  	"dates": "2005 - Present",
+	  	"url": "www.vcu.edu"
+	  }
+	],
+	"onlineCourses": [
+	  {
+	  	"title": "Introduction to Computer Science and Programming Using Python",
+	  	"school": "MIT",
+	  	"date": "March 2015",
+	  	"url": "https://courses.edx.org/courses/MITx/6.00.1x_5/1T2015/info"
+	  },
+	  {
+	  	"title": "An Introduction to the Intellectual Enterprises of Computer Science and the Art of Programming",
+	  	"school": "Harvard University",
+	  	"date": "March 2015",
+	  	"url": "https://www.edx.org/course/introduction-computer-science-harvardx-cs50x"
+	  },
+	  {
+	  	"title": "Developing Scalable Apps with Java",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud859"
+	  },
+	  {
+	  	"title": "Introduction to Point & Click App Development",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud162"
+	  },
+	  {
+	  	"title": "Introduction to Relational Databases",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud197"
+	  },
+	  {
+	  	"title": "Full Stack Foundations",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud088"
+	  },
+	  {
+	  	"title": "Web Development",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/cs253"
+	  },
+	  {
+	  	"title": "Responsive Web Design Fundamentals",
+	  	"school": "Udacity",
+	  	"date": "February 2015",
+	  	"url": "https://www.udacity.com/course/ud893"
+	  },
+	  {
+	  	"title": "JavaScript Design Patterns",
+	  	"school": "Udacity",
+	  	"date": "February 2015",
+	  	"url": "https://www.udacity.com/course/ud989"
+	  },	
+	  {
+	  	"title": "Introduction to Ajax",
+	  	"school": "Udacity",
+	  	"date": "February 2015",
+	  	"url": "https://www.udacity.com/course/ud110"
+	  },
+	  {
+	  	"title": "Website Performance Optimization",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud884"
+	  },
+	  {
+	  	"title": "HTML5 Canvas",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud292"
+	  },
+	  {
+	  	"title": "Object-Oriented JavaScript",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud015"
+	  },
+	  {
+	  	"title": "Intro to jQuery",
+	  	"school": "Udacity",
+	  	"date": "March 2015",
+	  	"url": "https://www.udacity.com/course/ud245"
+	  },
+	  { "title": "JavaScript Basics",
+	    "school": "Udacity",
+	    "date": "February 2015",
+	    "url": "https://www.udacity.com/course/ud804"
+	  },
+	  { "title": "Intro to Computer Science",
+	    "school": "Udacity",
+	    "date": "January 2015",
+	    "url": "https://www.udacity.com/course/cs101"
+	  },
+	  { "title": "Programming Foundation with Python",
+	    "school": "Udacity",
+	    "date": "January 2015",
+	    "url": "https://www.udacity.com/course/ud036"
+	  },
+	  { "title": "How to Use Git and GitHub",
+	    "school": "Udacity",
+	    "date": "January 2015",
+	    "url": "https://www.udacity.com/course/ud775"
+	  },
+	  { "title": "Intro to HTML and CSS",
+	    "school": "Udacity",
+	    "date": "January 2015",
+	    "url": "https://www.udacity.com/course/ud304"
+	  }
+	]
+};
+
+var work = {
+	"jobs": [
+	  {
+	  	"employer": "Innovation Tech",
+	  	"title": "Front-End Web Developer",
+	  	"location": "Ashburn, Virginia, United States",
+	  	"datesWorked": "September 2014 - Present",
+	  	"description": "Develop websites with an focus on the " + 
+	  	"front-end aspects such as HTML, CSS, and JavaScript to create responsive designed websites."
+	  },
+	  {
+	  	"employer": "Breedlove & Associates",
+	  	"title": "Private Childcare & Tutor",
+	  	"location": "Ashburn, Virginia, United States",
+	  	"datesWorked": "August 2008 - Present",
+	  	"description": "Provided private childcare and tutoring services to several families with children " +
+	  	"from the ages of newborn to 14 years old. I was responsible for the children's overall wellbeing " +
+	  	"while in my care, by providing stimulating educational and physical activities."
+	  },	
+	  {
+	  	"employer": "Virginia Commonwealth University",
+	  	"title": "Founder and Student Coordinator of RAMentoring",
+	  	"location": "Richmond, Virginia, United States",
+	  	"datesWorked": "August 2006 - May 2011",
+	  	"description": "As a undergraduate student at the Virginia Commonwealth University, I founded and " +
+	  	"served as the Student Coordinator of a student to student mentoring program. I fine tuned my request for proposal(RFP) writing skills " +
+	  	"as well as negotiation skills. I was successful in marketing and advertising this program throughout the university. " +
+	  	"I also developed my change management, project management, interviewing, application writing, resume reviewing, and leadership " +
+	  	"skills in this position."
+	  }, 
+	  {
+	  	"employer": "Virginia Commonwealth University",
+	  	"title": "Director of Finance",
+	  	"location": "Richmond, Virginia, United States",
+	  	"datesWorked": "May 2007 - May 2010",
+	  	"description": "Appointed by the 4 different elected student body president's " +
+	  	"consecutively for 4 years as the chief financial representative of the Student Government " +
+	  	"Association. Where I managed a working budget of over $50,000 yearly, payroll, time keeping, " +
+	  	"student-university financial relations. Most importantly I oversaw the allocation of over $4.2 million " +
+	  	"in student activity fee funds."
+	  },
+	  {
+	  	"employer": "Macy's",
+	  	"title": "Men's Customer Sales Associate",
+	  	"location": "Richmond, Virginia, United States",
+	  	"datesWorked": "August 2007 - May 2008",
+	  	"description": "Excelled at providing outstanding customer service and dispute resolution " +
+	  	"in a high sales volume environment. Consistently met weekly sales goals and promotions. " +
+	  	"Developed a high level of cash management skills and point-of-sales etiquette."
+	  },
+	  {	
+	  	"employer": "Virginia Commonwealth University",
+	  	"title": "Student Assistant to the Director of Advising",
+	  	"location": "Richmond, Virginia, United States",
+	  	"datesWorked": "August 2006 - May 2007",
+	  	"description": "Provided outstanding administrative and overall organizational support. I developed my time management and scheduling skills as I was " +
+	  	"responsible for scheduling advising appointments." 
+	  },
+	  {
+	  	"employer": "Virginia Commonwealth University",
+	  	"title": "Chairman of the Appropriations Committee",
+	  	"location": "Richmond, Virginia, United States",
+	  	"datesWorked": "August 2006 - May 2007",
+	  	"description": "Presided over a committee of 5 to 7 student body senators " +
+	  	"charged with the responsibility of allocating over $600,000 annually to campus " +
+	  	"student organizations."
+	  },
+	  {
+	  	"employer": "Virginia Commonwealth University",
+	  	"title": "Student Body Senator",
+	  	"location": "Richmond, Virginia, United States",
+	  	"datesWorked": "August 2005 - May 2007",
+	  	"description": "Elected by the largest student body in Virginia " +
+	  	"to serve as a student senator. Where I developed my leadership, " +
+	  	"negotiation, and problem solving skills to serve the student body. " +
+	  	"I served as Vice-Chairman of the Legislative Action and Civil Issues " +
+	  	"Committe, Vice-Chairman of the Academic Affairs Committee, Vice-Chairman " +
+	  	" of the Elections Committee, and Appropriations Committee member."
+	  },
+	  {
+	  	"employer": "Naval Sea Systems Command (NAVSEA) Dahlgren",
+	  	"title": "Physicist and Software Developer",
+	  	"location": "Dahlgren, Virginia, United States",
+	  	"datesWorked": "June 2006 - February 2007",
+	  	"description": "While in college I was selected for an co-op position " +
+	  	"with the J-14 Targeting and Vulnerability Analysis Branch."  +
+	  	"Where I learned C, C++, HTML, CSS, XTML, and how to develop " +
+	  	"robust dynamic military grade software. I also learned how to " +
+	  	"author help files for software programs to train our military and " +
+	  	"to use the software I helped develop and test."
+	  },
+	  {
+	  	"employer": "Movie Gallery",
+	  	"title": "Senior Customer Sales Associate",
+	  	"location": "Dahlgren, Virginia, United States",
+	  	"datesWorked": "December 2005 - August 2006",
+	  	"description": "At this movie and video game entertainment " +
+	  	"rental store, I was provided outstanding customer service, " +
+	  	"kept the store organized, recommended movies and video games, " +
+	  	"provided point-of-service sales, cash management, opened and closed " +
+	  	"the store."
+	  },
+	  {
+	  	"employer": "Naval Sea Systems Command (NAVSEA) Dahlgren",
+	  	"title": "Science and Engineering Intern",
+	  	"location": "Dahlgren, Virginia, United States",
+	  	"datesWorked": "June 2005 - August 2005",
+	  	"description": "I participated in the prestigious Science and Engineering Apprenticeship Program(SEAP), " +
+			"where I was selected to work for the N-14 Advanced Combat Systems Branch. " +
+			"I held Confidential Security Clearance to research and analyze the Ballistic Missile Defense System. " +
+			"This position developed my technical writing and research skills."
+	  },
+	  {
+	  	"employer": "Journal Press Inc.",
+	  	"title": "Webmaster",
+	  	"location": "King George, Virginia, United States",
+	  	"datesWorked": "July 2002 - February 2003",
+	  	"description": "I was charged with the sole responsibility of maintaining " +
+	  	"my county newspaper company's website. I successfully managed strict publication " +
+	  	"deadlines. Where I would take all the publications and update the website weekly. " +
+	  	"I mainly used HTML, CSS, and Coffeescript to manage the website."
+	  }
+	]
+}
+
+// TODO: figure out below 
+
+//function locationizer(work_obj) {
+	//var locationArray = [];
+
+	//for (job in work_obj.jobs) {
+		//var newLocation = work_obj.jobs[job].location;
+		//locationArray.push(newLocation);
+	//}
+
+	//return locationArray;
+//}
+
+//console.log(locationizer(work));
+
+var projects = {
+	"projects": [
+	  {
+ 	  	"title": "Mockup to Website",
+ 	  	"dates": "January 2015",
+ 	  	"description": "Will update soon",
+ 	  	"images": ["Under Construction", "Under Construction"],
+ 	  	"url": "tbd",
+ 	  },
+	  {
+ 	  	"title": "Sample Project 2",
+ 	  	"dates": "January 2015",
+ 	  	"description": "Will update soon",
+ 	  	"images": ["Under Construction", "Under Construction"],
+ 	  	"url": "tbd",
+ 	  },
+	  ]
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+var formattedContactInfo = [];
+formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+formattedContactInfo.push(HTMLtwitter.replace("%data%", bio.contacts.twitter));
+formattedContactInfo.push(HTMLlocation.replace("%data%",bio.contacts.location));
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(formattedBioPic);
+$("#header").append(formattedWelcomeMsg);
+
+if (bio.skills.length > 0) {
+	$("header").append(HTMLskillsStart);
+
+	for (job in bio.skills) {
+		$("#skills").append(HTMLskills.replace("%data%", bio.skills[job]));
+	}
+}
+
+for (job in formattedContactInfo){
+	$("#topContacts").append(formattedContactInfo[job]);
+	$("#footerContacts").append(formattedContactInfo[job]);
+}
+
+
+function displayWork() {
+
+	if (work.jobs.length > 0) {
+		  for(job in work.jobs){
+		  // create new div for work experience 
+		  $("#workExperience").append(HTMLworkStart);
+		  	// concatenate employer and title
+		  	var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		 	var formattedWorkTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		  	var formattedWorkLocation = HTMLworkLocation.replace("%data%",work.jobs[job].location);
+		  	var formattedDatesWorked = HTMLworkDates.replace("%data%", work.jobs[job].datesWorked);
+		  	var formattedWorkDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+
+		  	var formattedEmployerWorkTitle = formattedEmployer + formattedWorkTitle;
+		  
+		  	$(".work-entry:last").append(formattedEmployerWorkTitle);
+		  	$(".work-entry:last").append(formattedWorkLocation);
+		  	$(".work-entry:last").append(formattedDatesWorked);
+		  	$(".work-entry:last").append(formattedWorkDescription);
+	      }
+    }
+}
+
+displayWork();
+
+projects.display = function () {
+	if (projects.projects.length > 0) {
+		for (project in projects.projects) {
+			 // create new div for projects
+			 $("#projects").append(HTMLprojectStart);
+
+			 var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+			 var formattedProjectDates = HTMLprojectDates.replace("%data%",projects.projects[project].dates);
+			 var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+			 
+			 $(".project-entry:last").append(formattedProjectTitle);
+			 $(".project-entry:last").append(formattedProjectDates);
+			 $(".project-entry:last").append(formattedProjectDescription);
+	
+	    	for (image in projects.projects[project].images) {
+	    		var formattedProjectImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+	    		$(".project-entry:last").append(formattedProjectImage);
+			}
+		}
+	}
+}
+
+projects.display();
+
+education.display = function() {
+	if (education.schools.length > 0 || education.onlineCourses.length > 0) {
+		for (school in education.schools) {
+			$("#education").append(HTMLschoolStart);
+
+			var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name).replace("#", education.schools[school].url);
+			var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+			var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[school].majors);
+			
+
+			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDegree);
+			$(".education-entry:last").append(formattedSchoolDates);
+			$(".education-entry:last").append(formattedSchoolLocation);
+			$(".education-entry:last").append(formattedSchoolMajor);
+			
+		}
+
+		if (education.onlineCourses.length > 0) {
+			$("#education").append(HTMLonlineClasses);
+			for (school in education.onlineCourses) {
+				$("#education").append(HTMLschoolStart);
+				var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[school].title).replace("#", education.onlineCourses[school].url);
+				var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[school].school);
+				var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[school].date);
+				var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[school].url).replace("#", education.onlineCourses[school].url);
+				
+				$(".education-entry:last").append(formattedOnlineTitle + formattedOnlineSchool);
+				$(".education-entry:last").append(formattedOnlineDates);
+				$(".education-entry:last").append(formattedOnlineURL);
+			}	
+		}
+	}
+}
+
+education.display();
+
+//click location function
+$(document).click(function (loc) {
+    var x = loc.pageX;
+    var y = loc.pageY;
+
+    logClicks(x, y);
+});
+
+//internationalize my name button function
+function inName(name) {
+	name = name.trim().split(" ");
+	console.log(name);
+	name[1] = name[1].toUpperCase() + name[0].slice(1).tolowerCase();
+
+	return name[0] +" "+ name[1];
+}
+
+$('#main').append(internationalizeButton);
+
+//protected my resume from <sripts> 
+var charEscape = function(_html) {
+    var newHTML = _html;
+newHTML = _html.replace(/</g, "&lt;");
+    newHTML = newHTML.replace(/>/g, "&gt;");
+return newHTML;
+}
+//added map to my resume
+$("#mapDiv").append(googleMap);
